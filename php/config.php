9586103bb1bed6a007144b950422012accbd3606
Vega3 <danimt3705@gmail.com>
2024-06-13 09:47:00 -0500

    eliminamos comentario

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
 $db_host = "localhost";
 $db_user = "root";
-$db_pass = "new_secure_password"; // Asegúrate de usar una contraseña segura
+$db_pass = "new_secure_password"; 
 $db_name = "usuario_db";
