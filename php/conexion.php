33c10865517234512734b2c6db4d254f12a84f83
Vega3 <danimt3705@gmail.com>
2024-06-13 09:39:50 -0500

    Se añadio el archivo config.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,6 +1,7 @@
 <?php
+include 'config.php';
 
-$conexion = mysqli_connect("localhost", "root", "nalop21nlap","usuario_db");
+$conexion = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if($conexion){
     echo 'conecto existosamente a la base de datos';
---
+++ php/config.php
@@ -0,0 +1,5 @@
+<?php
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "new_secure_password"; // Asegúrate de usar una contraseña segura
+$db_name = "usuario_db";
